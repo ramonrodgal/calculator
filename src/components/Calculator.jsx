b5d70d08b04fec3be27ee064a32fe45b710d98e8
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-07 18:13:14 +0000

    Refactor Calculator to use EqualButton, remove form element ant input text

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,40 +4,41 @@ import { CalculatorContext } from "../contexts/CalculatorContext";
 import NumberButton from "./NumberButton";
 import OperatorButton from "./OperatorButton";
 import ClearButton from "./ClearButton";
+import EqualButton from "./EqualButton";
 
 export default function Calculator() {
-  const { input } = useContext(CalculatorContext);
+  const { input, memory } = useContext(CalculatorContext);
 
   return (
     <div>
-      <form>
-        <div>
-          <input type="text" value={input}></input>
-        </div>
-        <div>
-          <NumberButton value="7" />
-          <NumberButton value="8" />
-          <NumberButton value="9" />
-          <OperatorButton value="/" />
-        </div>
-        <div>
-          <NumberButton value="4" />
-          <NumberButton value="5" />
-          <NumberButton value="6" />
-          <OperatorButton value="*" />
-        </div>
-        <div>
-          <NumberButton value="1" />
-          <NumberButton value="2" />
-          <NumberButton value="3" />
-          <OperatorButton value="-" />
-        </div>
-        <div>
-          <ClearButton />
-          <NumberButton value="0" />
-          <OperatorButton value="+" />
-        </div>
-      </form>
+      <div>
+        <p>{memory}</p>
+        <p>{input}</p>
+      </div>
+      <div>
+        <NumberButton value="7" />
+        <NumberButton value="8" />
+        <NumberButton value="9" />
+        <OperatorButton value="/" />
+      </div>
+      <div>
+        <NumberButton value="4" />
+        <NumberButton value="5" />
+        <NumberButton value="6" />
+        <OperatorButton value="*" />
+      </div>
+      <div>
+        <NumberButton value="1" />
+        <NumberButton value="2" />
+        <NumberButton value="3" />
+        <OperatorButton value="-" />
+      </div>
+      <div>
+        <ClearButton />
+        <NumberButton value="0" />
+        <EqualButton />
+        <OperatorButton value="+" />
+      </div>
     </div>
   );
 }

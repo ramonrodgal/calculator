dc4e3de74e0354109fe9c7c51d02c4f0b0084c3a
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-07 18:11:50 +0000

    Add memory state and calculate function to CalculatorProvider

--- src/contexts/CalculatorContext.jsx
+++ src/contexts/CalculatorContext.jsx
@@ -4,9 +4,16 @@ export const CalculatorContext = createContext();
 
 export function CalculatorProvider({ children }) {
   const [input, setInput] = useState("");
+  const [memory, setMemory] = useState("");
+
+  const calculate = (value) => {
+    return eval(value);
+  };
 
   return (
-    <CalculatorContext.Provider value={{ input, setInput }}>
+    <CalculatorContext.Provider
+      value={{ input, setInput, memory, setMemory, calculate }}
+    >
       {children}
     </CalculatorContext.Provider>
   );

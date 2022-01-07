b76f36430bddd9c21bae43d67368db2ddd9e8131
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-07 17:51:20 +0000

    Refactor Calculator to use OperatorButton

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { CalculatorContext } from "../contexts/CalculatorContext";
+
 import NumberButton from "./NumberButton";
+import OperatorButton from "./OperatorButton";
 
 export default function Calculator() {
   const { input } = useContext(CalculatorContext);
@@ -15,16 +17,23 @@ export default function Calculator() {
           <NumberButton value="7" />
           <NumberButton value="8" />
           <NumberButton value="9" />
+          <OperatorButton value="/" />
         </div>
         <div>
           <NumberButton value="4" />
           <NumberButton value="5" />
           <NumberButton value="6" />
+          <OperatorButton value="*" />
         </div>
         <div>
           <NumberButton value="1" />
           <NumberButton value="2" />
           <NumberButton value="3" />
+          <OperatorButton value="-" />
+        </div>
+        <div>
+          <NumberButton value="0" />
+          <OperatorButton value="+" />
         </div>
       </form>
     </div>

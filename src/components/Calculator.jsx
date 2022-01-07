497460180cc8428f1103a135da7c991f11d67018
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-07 16:11:36 +0000

    Refactor Calculator component to use context

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { CalculatorContext } from "../contexts/CalculatorContext";
+import NumberButton from "./NumberButton";
 
 export default function Calculator() {
-  const [input, setInput] = useState("");
+  const { input } = useContext(CalculatorContext);
 
   return (
     <div>
       <form>
         <div>
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          ></input>
+          <input type="text" value={input}></input>
+        </div>
+        <div>
+          <NumberButton value="7" />
+          <NumberButton value="8" />
+          <NumberButton value="9" />
+        </div>
+        <div>
+          <NumberButton value="4" />
+          <NumberButton value="5" />
+          <NumberButton value="6" />
+        </div>
+        <div>
+          <NumberButton value="1" />
+          <NumberButton value="2" />
+          <NumberButton value="3" />
         </div>
       </form>
     </div>

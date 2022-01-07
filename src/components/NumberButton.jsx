77ab223dc61ee33232af204c1d637a8c4e866964
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-07 16:13:17 +0000

    Create NumberButton component

---
+++ src/components/NumberButton.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { CalculatorContext } from "../contexts/CalculatorContext";
+
+export default function NumberButton({ value }) {
+  const { setInput } = useContext(CalculatorContext);
+  return (
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        setInput((prevInput) => {
+          return prevInput + e.target.innerText;
+        });
+      }}
+    >
+      {value}
+    </button>
+  );
+}

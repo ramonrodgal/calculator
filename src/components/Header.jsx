3e599aa63354e4c7ba77d69dc5006509b05ffaf3
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-10 18:41:51 +0000

    Change Header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,7 @@ export default function Header() {
   return (
     <header className="m-6 p-2 text-cyan-200">
       <h1 className="text-center pb-6 text-6xl">
-        {" "}
-        <FontAwesomeIcon icon={faCalculator} /> - Simple Calculator
+        <FontAwesomeIcon icon={faCalculator} /> - Tiny Calculator
       </h1>
       <hr />
     </header>

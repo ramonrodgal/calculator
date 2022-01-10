961e15a06d1d567b91f188b1c772279b86d3e8bd
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-10 17:49:59 +0000

    Refactor OperatorButton to prevent adding multiple operators to input

--- src/components/buttons/OperatorButton.jsx
+++ src/components/buttons/OperatorButton.jsx
@@ -2,16 +2,24 @@ import React, { useContext } from "react";
 import { CalculatorContext } from "../../contexts/CalculatorContext";
 
 export default function OperatorButton({ value }) {
-  const { setInput } = useContext(CalculatorContext);
+  const { input, setInput, setMemory, calculate } =
+    useContext(CalculatorContext);
 
   return (
     <button
       className="basis-1/4 bg-cyan-700 m-2 p-2 rounded text-xl hover:bg-cyan-800"
       onClick={(e) => {
         e.preventDefault();
-        setInput((prevInput) => {
-          return prevInput + e.target.innerText;
-        });
+        if (input) {
+          setInput((prevInput) => {
+            if (prevInput.match(/[+\-*/]/g)) {
+              setMemory(prevInput);
+              return calculate(prevInput) + e.target.innerText;
+            } else {
+              return prevInput + e.target.innerText;
+            }
+          });
+        }
       }}
     >
       {value}

e54600705563a8aec53b9475f852d5e86bbbc8d2
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-10 18:42:12 +0000

    Wrap Calculator inside main tag

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -10,39 +10,41 @@ export default function Calculator() {
   const { input, memory } = useContext(CalculatorContext);
 
   return (
-    <div className=" bg-gray-500 box-border m-auto mt-12 mb-12 p-6 border-4 max-w-md rounded-xl">
-      <div className="flex flex-col">
-        <p className="m-2 p-1 text-right text-sm bg-cyan-100">
-          {memory ? memory : "Memory"}
-        </p>
-        <p className="m-2 p-1 text-right text-4xl bg-cyan-100">
-          {input ? input : "0"}
-        </p>
+    <main>
+      <div className=" bg-gray-500 box-border m-auto mt-12 mb-12 p-6 border-4 max-w-md rounded-xl">
+        <div className="flex flex-col">
+          <p className="m-2 p-1 text-right text-sm bg-cyan-100">
+            {memory ? memory : "Memory"}
+          </p>
+          <p className="m-2 p-1 text-right text-4xl bg-cyan-100">
+            {input ? input : "0"}
+          </p>
+        </div>
+        <div className="flex flex-row">
+          <NumberButton value="7" />
+          <NumberButton value="8" />
+          <NumberButton value="9" />
+          <OperatorButton value="/" />
+        </div>
+        <div className="flex flex-row">
+          <NumberButton value="4" />
+          <NumberButton value="5" />
+          <NumberButton value="6" />
+          <OperatorButton value="*" />
+        </div>
+        <div className="flex flex-row">
+          <NumberButton value="1" />
+          <NumberButton value="2" />
+          <NumberButton value="3" />
+          <OperatorButton value="-" />
+        </div>
+        <div className="flex flex-row">
+          <ClearButton />
+          <NumberButton value="0" />
+          <EqualButton />
+          <OperatorButton value="+" />
+        </div>
       </div>
-      <div className="flex flex-row">
-        <NumberButton value="7" />
-        <NumberButton value="8" />
-        <NumberButton value="9" />
-        <OperatorButton value="/" />
-      </div>
-      <div className="flex flex-row">
-        <NumberButton value="4" />
-        <NumberButton value="5" />
-        <NumberButton value="6" />
-        <OperatorButton value="*" />
-      </div>
-      <div className="flex flex-row">
-        <NumberButton value="1" />
-        <NumberButton value="2" />
-        <NumberButton value="3" />
-        <OperatorButton value="-" />
-      </div>
-      <div className="flex flex-row">
-        <ClearButton />
-        <NumberButton value="0" />
-        <EqualButton />
-        <OperatorButton value="+" />
-      </div>
-    </div>
+    </main>
   );
 }

c28f0d20289d0497e2a5a5171071436755015f88
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-10 18:05:03 +0000

    Prevent to add a number when the input is 0 or Infinity

--- src/components/buttons/NumberButton.jsx
+++ src/components/buttons/NumberButton.jsx
@@ -2,14 +2,19 @@ import React, { useContext } from "react";
 import { CalculatorContext } from "../../contexts/CalculatorContext";
 
 export default function NumberButton({ value }) {
-  const { setInput } = useContext(CalculatorContext);
+  const { input, setInput } = useContext(CalculatorContext);
   return (
     <button
       className="basis-1/4 bg-cyan-400 m-2 p-2 rounded text-xl hover:bg-cyan-500"
       onClick={(e) => {
         e.preventDefault();
+
         setInput((prevInput) => {
-          return prevInput + e.target.innerText;
+          if (prevInput === "0" || prevInput === "Infinity") {
+            return e.target.innerText;
+          } else {
+            return prevInput + e.target.innerText;
+          }
         });
       }}
     >

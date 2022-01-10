9e573d45aed02368b62814921593bcc4f99f55ec
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2022-01-10 21:04:44 +0000

    Limit the length of the calculator input

--- src/components/buttons/NumberButton.jsx
+++ src/components/buttons/NumberButton.jsx
@@ -2,20 +2,22 @@ import React, { useContext } from "react";
 import { CalculatorContext } from "../../contexts/CalculatorContext";
 
 export default function NumberButton({ value }) {
-  const { setInput } = useContext(CalculatorContext);
+  const { input, setInput } = useContext(CalculatorContext);
   return (
     <button
       className="basis-1/4 bg-cyan-400 m-2 p-2 rounded text-xl hover:bg-cyan-500"
       onClick={(e) => {
         e.preventDefault();
 
-        setInput((prevInput) => {
-          if (prevInput === "0" || prevInput === "Infinity") {
-            return e.target.innerText;
-          } else {
-            return prevInput + e.target.innerText;
-          }
-        });
+        if (input.length <= 10 || !/[0-9]$/g.test(input)) {
+          setInput((prevInput) => {
+            if (prevInput === "0" || prevInput === "Infinity") {
+              return e.target.innerText;
+            } else {
+              return prevInput + e.target.innerText;
+            }
+          });
+        }
       }}
     >
       {value}
